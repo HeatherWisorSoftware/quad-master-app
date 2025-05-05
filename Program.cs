a4e5b5a3d05700d1996c0ba41a3d3cd013aed9c8
Heather Wisor <heather@wisorsoftware.com>
2025-05-05 18:43:20 -0400

    Theme implemented for Quads

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using MudBlazor;
 using MudBlazor.Services;
 using QuadMasterApp.Components;
 using QuadMasterApp.Data;
+using QuadMasterApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,13 @@ builder.Services.AddScoped<DatabaseSeeder>();
 
 // Add MudBlazor services
 builder.Services.AddMudServices();
+
+// Register ThemeProvider as a singleton service
+// Create a default MudTheme to initialize the provider
+// The actual theme will be set by MainLayout when it initializes
+var defaultTheme = new MudTheme();
+builder.Services.AddSingleton<IThemeProvider>(serviceProvider =>
+    new ThemeProvider(defaultTheme, false));
 
 // Add services to the container.
 builder.Services.AddRazorComponents()

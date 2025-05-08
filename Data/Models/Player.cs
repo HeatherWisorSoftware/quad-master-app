cd6f7e3ba611d518db32b87c116694817dda063c
HeatherWisor <heather@wisorsoftware.com>
2025-05-07 21:11:36 -0400

    Updated Navigation, Created Page for uploading CSV Files, added styling a requirements for new player email and phone number in Add Memmber page

--- Data/Models/Player.cs
+++ Data/Models/Player.cs
@@ -7,6 +7,7 @@
         public string LastName { get;set; }
         public int Ranking { get; set; } 
         public string Email { get; set; }
+        public string Phone { get; set; }
 
         //Other player properties
         public List<TournamentPlayer> TournamentPlayers { get; set; }

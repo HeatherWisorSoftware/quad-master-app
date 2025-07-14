5eccc8f20231acd6a880435b3cf91af30752bb2f
Heather Wisor <heather@wisorsoftware.com>
2025-07-14 11:37:52 -0400

    trying to upload app to Digital Ocean

--- Program.cs
+++ Program.cs
@@ -9,108 +9,24 @@ using QuadMasterApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<TournamentContext>(options =>
-    options.UseSqlite("Data Source = tournament.db"));
-
-builder.Services.AddScoped<DatabaseSeeder>();
-
-// Add MudBlazor services
-builder.Services.AddMudServices();
-
-// Register ThemeProvider as a singleton service
-// Create a default MudTheme to initialize the provider
-// The actual theme will be set by MainLayout when it initializes
-var defaultTheme = new MudTheme();
-builder.Services.AddSingleton<IThemeProvider>(serviceProvider =>
-    new ThemeProvider(defaultTheme, false));
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-// Add global app state
-builder.Services.AddSingleton<AppStateService>();
-
-// no authentication in development mode
-if (!builder.Environment.IsDevelopment())
-{
-    // Add Microsoft Identity authentication
-    builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-        .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
-}
-
-// Add authorization with email restrictions
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("AllowedUsers", policy =>
-        policy.RequireAssertion(context =>
-        {
-            // List of allowed email addresses
-            var allowedEmails = new[]
-            {
-                "heather@wisorsoftware.com",
-                "bobscaccia62@gmail.com",
-                "shawnie@wisorsoftware.com",
-                "bob.scaccia@usafirmware.com",
-                "jason.hoagland.jr@gmail.com"
-            };
-
-            // Get the user's email from the authentication claims
-            var userEmail = context.User.FindFirst("preferred_username")?.Value
-                            ?? context.User.FindFirst("email")?.Value
-                            ?? context.User.Identity?.Name;
-
-            // Check if user's email is in the allowed list
-            return allowedEmails.Contains(userEmail, StringComparer.OrdinalIgnoreCase);
-        }));
-});
+// Add services
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 
 var app = builder.Build();
 
-var logger = app.Services.GetRequiredService<ILogger<Program>>();
-
-// Get database reset configuration from appsettings.json
-var resetDatabase = builder.Configuration.GetValue<bool>("DatabaseOptions:ResetOnStartup");
-
-// Initialize the database with the configuration setting
-await app.Services.InitializeDatabaseAsync(logger, resetDatabase);
-
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseExceptionHandler("/Error");
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
-if (!app.Environment.IsDevelopment())
-{
-    // Use authentication only in production
-    app.UseAuthentication();
-}
+app.MapRazorPages();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
-app.UseAuthorization();
-app.UseAntiforgery();
-
-app.MapStaticAssets();
-
-if (!app.Environment.IsDevelopment())
-{
-    app.MapRazorComponents<App>()
-        .AddInteractiveServerRenderMode()
-        .RequireAuthorization("AllowedUsers");
-
-    app.Urls.Add("http://localhost:5000");
-    app.Urls.Add("https://localhost:5001");
-}
-else
-{
-    app.MapRazorComponents<App>()
-        .AddInteractiveServerRenderMode();
-}
-
-app.Run();
+// Important: Listen on all interfaces
+app.Run("http://0.0.0.0:80");
